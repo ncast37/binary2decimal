7611fd11d192db7483d5df67a115e451d0ccbbf2
nickC <nick.castillo37@gmail.com>
2024-03-16 15:11:49 -0800

    Add Navbar to page

--- project/src/app/page.js
+++ project/src/app/page.js
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import Container from "@mui/material/Container";
+import NaviationBar from "./Components/Navigation/NavigationBar";
+import Box from "@mui/material/Box";
 
 export default function Home() {
-  return <Container> Hello World</Container>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "transparent",
+      }}
+    >
+      <NaviationBar />
+    </Box>
+  );
 }
