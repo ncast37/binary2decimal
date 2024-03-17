e3082de6f40480c993654fc8b6e8b9d139a100c5
nickC <nick.castillo37@gmail.com>
2024-03-17 14:25:57 -0800

    feat: Add onChangeInput prop to manage parent state.
    The onChangeInput prop function is called via useEffect hook when inputValue changes.

--- project/src/app/Components/ConverterFeature/TextField.jsx
+++ project/src/app/Components/ConverterFeature/TextField.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { Box, TextField, Typography } from "@mui/material";
 
-export default function BinaryInputField({ digit, index }) {
+export default function BinaryInputField({ digit, index, onInputChange }) {
   // False represents the digit 0
   // True represents the digit 1
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("0");
   const [isValid, setIsValid] = useState(true);
 
   const handleEvent = (e) => {
     const v = e.key;
-    setInputValue(v === "1" || v === "0" ? v : "");
+    setInputValue(v === "1" ? "1" : "0");
   };
+
+  useEffect(() => {
+    onInputChange(index, inputValue);
+  }, [inputValue]);
 
   return (
     <Box
